2555ee90e1341b271a47b0d1a950830137044dde
Uday <budayteja009@gmail.com>
2022-01-23 00:11:08 +0530

    final commit

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const app = express();
 const port = 3000;
 const apiRouter = require('./routes/api/v1/index');
 
-app.use('/api/v1', apiRouter);
 
 // parse application/x-www-form-urlencoded
 
@@ -13,6 +12,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json());
 
+app.use('/api/v1', apiRouter);
 
 // app.use(express.urlencoded({ limit: '50mb', extended: false }));
 
