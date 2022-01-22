17a42b711b4ed57e47f186c750dc8de9dcca4d88
Uday <budayteja009@gmail.com>
2022-01-23 00:00:28 +0530

    added req body-parser

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+var bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
 const apiRouter = require('./routes/api/v1/index');
 
 app.use('/api/v1', apiRouter);
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
+// parse application/x-www-form-urlencoded
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json());
+
+
+// app.use(express.urlencoded({ limit: '50mb', extended: false }));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
