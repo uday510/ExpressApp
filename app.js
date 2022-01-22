f4ca51c2486bd7fed60d5391a920df31762faf80
Uday <budayteja009@gmail.com>
2022-01-22 22:48:40 +0530

    added routing

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const apiRouter = require('./routes/api/v1/index');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use('/api/v1', apiRouter);
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
